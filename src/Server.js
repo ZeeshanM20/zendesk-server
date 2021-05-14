91c1e1f5f879b6ce7a4d101fe6fa5229dd4747c0
ZeeshanMujtaba <zmujtaba96@gmail.com>
2021-05-14 16:38:41 +1000

    Display tickets

--- src/Server.js
+++ src/Server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { getTickets } = require('./tickets')
+const fs = require('fs')
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,16 @@ app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.get('/tickets', getTickets);
+app.get('/tickets', async (req, res) => {
+  const tickets = await getTickets()
+  res.set('Content-Type', 'application/json')
+  res.send(tickets)
+});
+app.get('/', (req, res)=> {
+  const index = fs.readFileSync('./index.html')
+  // console.log(index);
+  res.set('Content-Type', 'text/html')
+  res.send(index)
+});
 
 app.listen(port, () => console.log(`Server running on ${port}!`));
--- src/tickets.js
+++ src/tickets.js
@@ -2,17 +2,19 @@ const fetch = require('node-fetch')
 
 const getTickets = async () => {
 
-let url = 'https://zeeshan-mujtaba.zendesk.com/api/v2/tickets.json'
+  let url = 'https://zeeshan-mujtaba.zendesk.com/api/v2/tickets.json'
 
-let options = {
-  method: 'GET',
-  headers: {'Authorization': 'Basic ' + btoa('zmujtaba96@gmail.com:**********')}
-};
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
+  let options = {
+    method: 'GET',
+    headers: {'Authorization': 'Basic ' + btoa('zmujtaba96@gmail.com:Zendesk123')}
+  };
+  try{
+    const response = await fetch(url, options)
+      return response.json()
+  }
+  catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = { getTickets }
