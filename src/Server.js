9de37bf8e56e78c461ec90bb93594fa0307c148d
ZeeshanMujtaba <zmujtaba96@gmail.com>
2021-05-14 22:16:45 +1000

    Add pagination

--- src/Server.js
+++ src/Server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors');
 const { getTickets } = require('./tickets')
+const { getPrevPage } = require('./prevPage')
+const { getNextPage } = require('./nextPage')
 const fs = require('fs')
 
 const app = express();
@@ -15,9 +17,18 @@ app.get('/tickets', async (req, res) => {
   res.set('Content-Type', 'application/json')
   res.send(tickets)
 });
+app.get('/prevPage', async (req, res) => {
+  const tickets = await getPrevPage(req.query.url)
+  res.set('Content-Type', 'application/json')
+  res.send(tickets)
+});
+app.get('/nextPage', async (req, res) => {
+  const tickets = await getNextPage(req.query.url)
+  res.set('Content-Type', 'application/json')
+  res.send(tickets)
+});
 app.get('/', (req, res)=> {
   const index = fs.readFileSync('./index.html')
-  // console.log(index);
   res.set('Content-Type', 'text/html')
   res.send(index)
 });
